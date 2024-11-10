08fa4e106c58575727baafd64e59fa4b5a9fc177
Benjamin NIDDAM <benjamin@weaverize.com>
2024-11-10 21:02:39 +0100

    feat: 🎸 add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,33 @@
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Fullstack auth')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
+  const logger = new Logger();
 
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('PORT');
+
+  logger.verbose(`Server is running on http://localhost:${port}/api`);
 
   await app.listen(port);
 }
