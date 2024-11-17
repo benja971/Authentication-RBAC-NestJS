25c008cff7fc2fd5d547507bd17290b95fdcf38a
Benjamin NIDDAM <benjamin@weaverize.com>
2024-11-17 19:21:41 +0100

    refactor: 💡 Simplify role assignment logic and enforce admin role requirement

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,9 @@
 import { Body, Controller, Param, Patch, Req } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthenticatedRequest } from 'src/auth/auth.guard';
-import {
-  AssignRoleToUserDto,
-  AssignRoleToUserParams,
-} from 'src/roles/dto/assign-role.dto';
+import { AssignRoleToUserDto, AssignRoleToUserParams } from 'src/roles/dto/assign-role.dto';
+import { Roles } from 'src/roles/roles.decorator';
+import { Role } from 'src/roles/roles.enum';
 import { RolesService } from 'src/roles/roles.service';
 import { UsersService } from './users.service';
 
@@ -16,16 +15,12 @@ export class UsersController {
   ) {}
 
   @ApiBearerAuth()
+  @Roles(Role.Admin)
   @Patch(':userId/roles')
-  async assignRoleToUser(
-    @Req() request: AuthenticatedRequest,
-    @Param() { userId }: AssignRoleToUserParams,
-    @Body() { role }: AssignRoleToUserDto,
-  ) {
+  async assignRoleToUser(@Req() request: AuthenticatedRequest, @Param() { userId }: AssignRoleToUserParams, @Body() { role }: AssignRoleToUserDto) {
     const { user } = request;
 
     this.rolesService.assertNoSelfAssignment(userId, user.id);
-    this.rolesService.assertAssignerIsAdmin(user);
 
     return this.usersService.assignRoleToUser(userId, role);
   }
