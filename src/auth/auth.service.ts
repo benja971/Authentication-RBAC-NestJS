a21341bac6d3e1c95a155703810a3748044de591
Benjamin NIDDAM <benjamin@weaverize.com>
2024-12-08 15:40:34 -0400

    feat: ✨ Add logout and refresh token functionality with validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { JwtUserDto } from 'src/commons/types';
 import { NotificationType } from 'src/notifications/notification-types.enum';
 import { NotificationsGateway } from 'src/notifications/notifications.gateway';
 import { AccessTokensService } from 'src/tokens/access_tokens.service';
@@ -42,6 +43,33 @@ export class AuthService {
 
     // Sauvegarde du refresh token
     await this.refreshTokenService.create(user.id, refreshToken);
+
+    return {
+      accessToken,
+    };
+  }
+
+  async logout(userId: string): Promise<void> {
+    this.logger.verbose(`Logout user with ID: ${userId}`);
+
+    // Suppression du refresh token
+    await this.refreshTokenService.deleteByUser(userId);
+  }
+
+  async refresh(jwtUserDto: JwtUserDto): Promise<{ accessToken: string }> {
+    this.logger.verbose(`Refresh token for user with ID: ${jwtUserDto.id}`);
+
+    // Récupération du refresh token
+    const refreshToken = await this.refreshTokenService.findByUserId(jwtUserDto.id);
+
+    // Vérification du refresh token
+    if (!this.refreshTokenService.verify(refreshToken.token)) {
+      this.logger.error(`Invalid refresh token for user with ID: ${jwtUserDto.id}`);
+      throw new UnauthorizedException('Invalid refresh token');
+    }
+
+    // Génération d'un nouveau access token
+    const accessToken = this.accessTokenService.generate(jwtUserDto);
 
     return {
       accessToken,
